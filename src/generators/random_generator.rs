6497418191aafc13ff043aec8d5b6c239f13ae34
smol-cat <ernestasunt@gmail.com>
2024-02-24 10:10:22 +0200

    added mass center calculation

--- src/generators/random_generator.rs
+++ src/generators/random_generator.rs
@@ -24,14 +24,14 @@ pub fn generate_two_clusters(count: u32) -> Vec<Vertex> {
     for _ in 0..(count / 2) {
         vertices.push(Vertex::new(
             vec2(rng.gen_range(-0.8..-0.3), rng.gen_range(-0.5..0.5)),
-            vec2(0.0, 0.0), //vec2(rng.gen_range(-0.001..0.001), rng.gen_range(-0.001..0.001)),
+            vec2(rng.gen_range(-0.003..0.003), rng.gen_range(-0.003..0.003)),
         ));
     }
 
     for _ in 0..(count / 2) {
         vertices.push(Vertex::new(
             vec2(rng.gen_range(0.3..0.8), rng.gen_range(-0.5..0.5)),
-            vec2(0.0, 0.0), //vec2(rng.gen_range(-0.001..0.001), rng.gen_range(-0.001..0.001)),
+            vec2(rng.gen_range(-0.003..0.003), rng.gen_range(-0.003..0.003)),
         ));
     }
 
